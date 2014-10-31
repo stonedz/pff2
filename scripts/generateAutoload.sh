ef9551dd3c7aba8ba9939cefae0c69c15061c470
Paolo Fagni <paolo.fagni@gmail.com>
2014-10-31 14:52:37 +0100

    autoload only for the app dir

--- scripts/generateAutoload.sh
+++ scripts/generateAutoload.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-tmp=`pwd`
-cd vendor/stonedz/pff2
-phpab --exclude 'lib/vendor/ezyang/htmlpurifier/maintenance/*' \
-    --exclude 'lib/vendor/doctrine/common/tests*' \
-    --exclude 'lib/vendor/doctrine/dbal/tests*' \
-    --exclude 'lib/vendor/doctrine/orm/tests*' \
-    --exclude 'lib/vendor/smarty/smarty/development*' \
-    --exclude 'lib/vendor/swiftmailer/swiftmailer/test-suite*' \
-    --exclude 'lib/vendor/swiftmailer/swiftmailer/tests*' \
-    --exclude '*/tests/*' \
-    --output src/autoload.php src 
-cd $tmp
-
 phpab --exclude 'app/vendor/doctrine/common/tests*' \
     --exclude 'app/vendor/doctrine/dbal/tests*' \
     --exclude 'app/vendor/doctrine/orm/tests*' \
