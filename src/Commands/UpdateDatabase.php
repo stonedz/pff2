884c83a7019cd3b7a38d08b8998426dae2becfbb
Paolo Fagni <paolo.fagni@gmail.com>
2015-02-05 11:17:21 +0100

    Update Db command for doctrine

--- src/Commands/UpdateDatabase.php
+++ src/Commands/UpdateDatabase.php
@@ -9,7 +9,9 @@ namespace pff\Commands;
 
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class UpdateDatabase extends Command {
@@ -17,11 +19,25 @@ class UpdateDatabase extends Command {
     protected function configure() {
         $this
             ->setName('db:updateDb')
-            ->setDescription('Backups and then updates the db using doctrine orm:schema-tool:update --force');
+            ->setDescription('Backups and then updates the db using doctrine orm:schema-tool:update --force')
+            ->addOption(
+                'no-backup',
+                null,
+                InputOption::VALUE_NONE
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
-        exec('vendor/bin/doctrine',$res);
+        if(!$input->getOption('no-backup')) {
+
+            $command   = $this->getApplication()->find('db:backupDb');
+            $arguments = array('command' => 'db:backupDb');
+            $inputa    = new ArrayInput($arguments);
+            $ret       = $command->run($inputa, $output);
+
+        }
+
+        exec('vendor/bin/doctrine orm:schema-tool:update --force',$res);
         foreach ($res as $r) echo $r."\n";
     }
 }
