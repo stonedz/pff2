59a76a39a098162efa0ced05e69d8ac6f24793af
Paolo Fagni <paolo.fagni@gmail.com>
2016-03-31 14:19:36 +0200

    better HTTPS detection

--- resources/site_skeleton/index.php
+++ resources/site_skeleton/index.php
@@ -12,7 +12,16 @@ define('ROOT', __DIR__);
 define('ROOT_LIB', __DIR__.DS.'vendor'.DS.'stonedz'.DS.'pff2');
 define('CONTROLLERS', __DIR__ . DS. 'app'.DS.'controllers');
 define('PAGES', __DIR__  .DS.'app'.DS.'pages');
-$protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != "off") ? "https" : "http";
+
+$isSecure = false;
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+    $isSecure = true;
+}
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on') {
+    $isSecure = true;
+}
+$protocol = $isSecure ? 'https' : 'http';
+
 $ext_root =  $protocol . "://" . $_SERVER['HTTP_HOST'].'/';
 
 define('EXT_ROOT', $ext_root);
