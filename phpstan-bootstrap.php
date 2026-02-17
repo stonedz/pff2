5ea59d44a09af40e4e1a8731211168b812906706
Paolo Fagni <paolo.fagni@gmail.com>
2026-02-17 11:30:08 +0100

    ci: add phpstan configuration and baseline

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
+if (!defined('ROOT')) {
+    define('ROOT', __DIR__);
+}
+if (!defined('ROOT_LIB')) {
+    define('ROOT_LIB', __DIR__);
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
